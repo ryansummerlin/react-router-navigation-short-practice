33bc69586648af282c93a8c18aaafa045f39c813
Ryan Summerlin <ryansummerlin777@gmail.com>
2023-06-13 12:09:29 -0700

    Initial commit

---
+++ src/components/Stocks/index.js
@@ -0,0 +1,23 @@
+import { Redirect, useHistory } from "react-router-dom";
+
+function Stocks() {
+  const loggedIn = true;
+
+  const history = useHistory();
+
+  const handleClick = () => {
+    window.alert("Sending info to the DB!");
+    history.push("/");
+  };
+
+  if (loggedIn === false) return <Redirect to="/not-logged-in" />;
+
+  return (
+    <div className="comp orange">
+      <h1>Stocks Component</h1>
+      <button onClick={handleClick}>Home</button>
+    </div>
+  );
+}
+
+export default Stocks;
